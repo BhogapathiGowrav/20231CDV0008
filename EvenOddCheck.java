f86a062fc89cd4998df0685e8fa1446254fc4d8d
BhogapathiGowrav <gowrav.bhogapathi@gmail.com>
2026-03-04 11:44:49 +0530

    Update EvenOddCheck.java

--- EvenOddCheck.java
+++ EvenOddCheck.java
@@ -8,7 +8,7 @@ public class EvenOddCheck {
         scanner.close();
 
         if (num % 2 == 0) {
-            System.out.println(num + " is even.");
+            System.out.println(num + " is even.")
         } else {
             System.out.println(num + " is odd.");
         }
